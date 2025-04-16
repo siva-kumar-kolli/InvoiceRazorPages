7269b36f2face5059b1bbb5a580a45351c9e6ba2
siva kumar kolli <104115777+siva-kumar-kolli@users.noreply.github.com>
2025-04-16 09:59:47 +0530

    upadated

---
+++ LonrixApp/Pages/Invoices/Map.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LonrixApp.Pages.Invoices
+{
+    public class MapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LonrixApp/Pages/Invoices/destinationMap.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LonrixApp.Pages.Invoices
+{
+    public class MapyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
